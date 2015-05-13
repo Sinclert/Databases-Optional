deacddcc545bc984aec84dd0b92ce89d14cf63ac
Daniel Fernandez Rico <deferrico@gmail.com>
2015-05-13 13:56:55 +0200

    'FFP to int' and 'int to FFP' methods finished and working (almost). Main problems: 1. On FFP to int: as we take the 1st two bits of the 1st byte, we don't care about the value of the byte. It's the same result for 3, 127, -125... 2. On int to FFP: the same as 1, plus as we don't care about the sign of the byte when converting to int, when we do the opposite we assume that it's positive. That means that negative bytes are transformed to its corresponding positive value (+128).

--- src/Serial.java
+++ src/Serial.java
@@ -163,7 +163,7 @@ public class Serial {
 
 
     public int readFFP(int bucket) throws IOException {
-        byte[] block = new byte[BLOCKSIZE+2];
+        byte[] block = new byte[BLOCKSIZE + 2];
         f.read(block);
         return bucket;
     }
@@ -172,21 +172,20 @@ public class Serial {
         return bucket;
     }
 
-    public int FFPtoInt(byte ffps []){
+    public int FFPtoInt(byte[] ffps) {
         int[] array = new int[10];
 
         int ffps0 = (int) ffps[0] + 128;
-        for (int i = 9; i > 1; i--) {
+        for (int i = 1; i >= 0; i--) {
             array[i] = ffps0 % 2;
             ffps0 = ffps0 / 2;
         }
 
         int ffps1 = (int) ffps[1] + 128;
-        for (int i = 1; i >= 0; i--) {
+        for (int i = 9; i > 1; i--) {
             array[i] = ffps1 % 2;
             ffps1 = ffps1 / 2;
         }
-
         /*
          * 0 1 2 3 4 5 6 7 8 9
          * 0 - 128 - 256
@@ -198,8 +197,31 @@ public class Serial {
             if (i != 2) sum += array[i] * (Math.pow(2, j));
             else j--;
         }
-        System.out.println(Arrays.toString(array));
-        System.out.println(sum);
         return sum;
+    }
+
+    public byte[] intToFFP(int number) {
+        String inttoS = Integer.toBinaryString(number);
+        int[] array = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+        for (int i = 0, j = inttoS.length() - 1, k = 9; i < inttoS.length(); i++, j--, k--) {
+            if (k != 2) array[k] = Integer.parseInt("" + inttoS.charAt(j));
+            else {
+                i--;
+                j++;
+            }    //intentamos replicar el array anterior, donde concatenamos los dos bytes
+        }
+        array[2] = 1;           //problemas con el signo, mas o menos
+        int ffps21 = 0;
+        for (int i = 1, j = 0; i >= 0; i--, j++) {
+            ffps21 += array[i] * (Math.pow(2, j));
+        }
+        //ffps21 -= 128;
+        int ffps22 = 0;
+        for (int i = 9, j = 0; i > 2; i--, j++) {
+            ffps22 += array[i] * (Math.pow(2, j));
+        }
+        //if(ffps22 < 128)ffps22 -= 128;
+        return new byte[]{(byte) ffps21, (byte) ffps22};
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -7,17 +7,17 @@ import java.util.Arrays;
  */
 public class Test {
     public static void main(String[] args) {
-        byte[] ffps = {1, -127}; // 128, 255 = 384
+        byte[] ffps = {3, -100}; // 128, 255 = 384
         int[] array = new int[10];
 
         int ffps0 = (int) ffps[0] + 128;
-        for (int i = 9; i > 1; i--) {
+        for (int i = 1; i >= 0; i--) {
             array[i] = ffps0 % 2;
             ffps0 = ffps0 / 2;
         }
 
         int ffps1 = (int) ffps[1] + 128;
-        for (int i = 1; i >= 0; i--) {
+        for (int i = 9; i > 1; i--) {
             array[i] = ffps1 % 2;
             ffps1 = ffps1 / 2;
         }
@@ -35,5 +35,33 @@ public class Test {
         }
         System.out.println(Arrays.toString(array));
         System.out.println(sum);
+        /////////////////////////////////
+
+        String inttoS = Integer.toBinaryString(sum);
+        System.out.println(Integer.toBinaryString(sum));
+        int[] array2 = {0,0,0,0,0,0,0,0,0,0};
+        for (int i = 0, j=inttoS.length()-1, k = 9; i < inttoS.length(); i++, j--, k--) {
+            if(k!=2) array2[k] = Integer.parseInt(""+inttoS.charAt(j));
+            else {i--; j++;}
+        }
+        array2[2] = 1;
+        System.out.println(Arrays.toString(array2));
+
+        int ffps21 = 0;
+        for (int i = 1, j = 0; i >= 0; i--, j++) {
+            ffps21 += array2[i] * (Math.pow(2, j));
+        }
+        //ffps21 -= 128;
+        System.out.println(ffps21);
+
+        int ffps22 = 0;
+        for (int i = 9, j = 0; i > 2; i--, j++) {
+            ffps22 += array2[i] * (Math.pow(2, j));
+        }
+        //if(ffps22 < 128)ffps22 -= 128;
+        System.out.println(ffps22);
+
+        byte[]  ffpsdef = {(byte)ffps21,(byte)ffps22};
+        System.out.println(Arrays.toString(ffpsdef));
     }
 }
